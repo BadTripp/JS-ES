89a609ba43cd72900e31f0f6a9f9ecb4063b8a5a
BadTripp <renato.turetta00@gmail.com>
2022-03-28 10:03:03 +0200

    c

--- area.js
+++ area.js
@@ -1,16 +1,8 @@
 
-
-
-
-
-
-
-
 function calcola(){
     var base=document.getElementById("txbase").value;
     var altezza=document.getElementById("txaltezza").value;
     var select=document.getElementById("selezioneArea").value;
     if(select=="Quadrato"){var area=base*altezza;}else{var area=(base*altezza)/2;}
     document.getElementById("risultato").innerHTML = "Area:"+area;
-    console.log("calcola");
 }
