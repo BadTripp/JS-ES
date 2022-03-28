c82de190b7b9948bdc5c3274a8399d81d2a718e4
BadTripp <renato.turetta00@gmail.com>
2022-03-28 16:26:18 +0200

    bomba fix

--- bomba.js
+++ bomba.js
@@ -1,23 +1,30 @@
-let timer=0;
-let i=0;
-let id=0;
-let end=0;
-function esplosione (){
-    timer=document.getElementById("timer").value
-    if(timer >100 || timer < 0 || end != 0 ){document.getElementById("rovescia").innerText="Inserisci un valore maggiore di 1 e non superiore a 100";}
-    else {id=setInterval(conto,1000,[timer]); end=1;}
-    
-    
+var timer = 0;
+let i = 0;
+let id = 0;
+let end = 0;
+function esplosione() {
+  i = 0;
+  timer = document.getElementById("timer").value;
+  if (timer > 100 || timer < 0 || end != 0) {
+    document.getElementById("rovescia").innerText =
+      "Inserisci un valore maggiore di 1 e non superiore a 100";
+  } else {
+    id = setInterval(conto, 1000, [timer]);
+    end = 1;
+  }
 }
-function conto(t){ 
-    end=1; 
-    i++
-    document.getElementById("rovescia").innerText=t-i;
-    
-    if(i == t) {
-        clearInterval(id);
-        document.getElementById("imgBomba").src = "./immagini/esplosione.jpg";
-        end=0;
-         };
-    
+function conto(t) {
+  document.getElementById("button").disabled = true;
+  end = 1;
+  i++;
+  document.getElementById("rovescia").innerText = t - i;
+  if (i == t) {
+    clearInterval(id);
+    document.getElementById("button").disabled = false;
+    document.getElementById("imgBomba").src = "./immagini/esplosione.jpg";
+    end = 0;
+  }
+}
+function clearInput() {
+  document.getElementById("timer").value = "";
 }
