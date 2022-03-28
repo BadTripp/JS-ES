78a92ca8ea99ed3c25bc1ef0f884fc3c604c67f0
BadTripp <renato.turetta00@gmail.com>
2022-03-28 16:03:29 +0200

    bomba

--- bomba.js
+++ bomba.js
@@ -1,15 +1,23 @@
 let timer=0;
-let i=1;
+let i=0;
 let id=0;
+let end=0;
 function esplosione (){
     timer=document.getElementById("timer").value
-    id=setInterval(conto,1000,[document.getElementById("timer").value]);
+    if(timer >100 || timer < 0 || end != 0 ){document.getElementById("rovescia").innerText="Inserisci un valore maggiore di 1 e non superiore a 100";}
+    else {id=setInterval(conto,1000,[timer]); end=1;}
+    
     
 }
-function conto(t){  
+function conto(t){ 
+    end=1; 
     i++
-    if(i >= t) {clearInterval(id);
+    document.getElementById("rovescia").innerText=t-i;
+    
+    if(i == t) {
+        clearInterval(id);
         document.getElementById("imgBomba").src = "./immagini/esplosione.jpg";
+        end=0;
          };
     
 }
