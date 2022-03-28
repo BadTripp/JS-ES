8ec5b59aa45fd69f4dc0a75982ad15a487c90a36
BadTripp <renato.turetta00@gmail.com>
2022-03-28 11:54:41 +0200

    gelati es

--- area.js
+++ area.js
@@ -1,11 +1,13 @@
 function calcola() {
   var base = document.getElementById("txbase").value;
   var altezza = document.getElementById("txaltezza").value;
-  var select = document.getElementById("selezioneArea").value;
-  if (select == "Rettangolo") {
-    var area = base * altezza;
-  } else {
-    var area = (base * altezza) / 2;
-  }
-  document.getElementById("risultato").innerHTML = "Area:" + area;
+  var select = document.getElementById("selezioneArea");
+  var area;
+  area =(select.value=="Rettangolo")?  area = base * altezza :  area = (base * altezza) / 2;
+//   if (select == "Rettangolo") {
+//     var area = base * altezza;
+//   } else {
+//     var area = (base * altezza) / 2;
+//   }
+  document.getElementById("risultato").innerHTML = "Area:"+select.textContent.length + area;
 }
---
+++ array.js
@@ -0,0 +1,8 @@
+var gelati=["nocciola","cioccolato","fragola","pistacchio","cocco"];
+var numeroGusti=gelati.length;
+var ul=document.getElementById("listaGelati");
+var li=document.createElement("li");
+var text;
+        for(let i=100;i >= 0 ;i--){
+            console.log(i)
+        }
