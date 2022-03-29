3f5eae3da1abef9048f765880ee80e7b0ba32c80
BadTripp <renato.turetta00@gmail.com>
2022-03-29 10:41:20 +0200

    Update numeri.js

--- numeri.js
+++ numeri.js
@@ -4,24 +4,28 @@ let match=0;
 let mostraNumero = function (){
     
     numeroUtente=document.getElementById("input").value;
-    if(numeroUtente > 0)
+    if(numeroUtente > 0 )
     {
     numeroRandom=Math.floor(Math.random() * 6) + 1;
     match++;
     mostraDado(numeroRandom);
-    if(numeroUtente == numeroRandom){
-        score++;
-        document.getElementById("risultato").style.color = "green";
-        document.getElementById("risultato").innerText="Hai indovinato!- Numero uscito:"+numeroRandom;
-    }else{ document.getElementById("risultato").style.color = "red"; document.getElementById("risultato").innerText="Non hai indovinato  indovinato!- Numero uscito:"+numeroRandom;}
+    check(numeroUtente,numeroRandom);
     document.getElementById("score").innerText="Score"+score+"\n"+"Giocate:"+match;
     //document.getElementById("risultato").innerText= Math.round(numero);
     // document.getElementById("risultato").innerText= Math.ceil(numero);
     // document.getElementById("risultato").innerText= Math.floor(numero);
     // document.getElementById("risultato").innerText= numero.tofixed(2);
 }else {document.getElementById("risultato").innerText="Inserisci un numero maggiore di 0"}
+    numeroUtente=document.getElementById("input").value="";
 }
 function mostraDado(numero){
 
     document.getElementById("imgDado").src="./immagini/dado"+numero+".png";
+}
+function check(numeroUtente,numeroRandom){
+    if(numeroUtente == numeroRandom){
+        score++;
+        document.getElementById("risultato").style.color = "green";
+        document.getElementById("risultato").innerText="Hai indovinato!- Numero uscito:"+numeroRandom;
+    }else{ document.getElementById("risultato").style.color = "red"; document.getElementById("risultato").innerText="Non hai indovinato  indovinato!- Numero uscito:"+numeroRandom;}
 }
