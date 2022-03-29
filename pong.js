83aa883acf1b28733df356797119050ea007827b
BadTripp <renato.turetta00@gmail.com>
2022-03-29 13:01:20 +0200

    pong loop

--- pong.js
+++ pong.js
@@ -2,11 +2,18 @@ pallina=document.getElementById("pallina");
 tela=document.getElementById("tela");
 stopB=document.getElementById("buttonStop");
 startB=document.getElementById("button");
+pallinaLeft=pallina.offsetLeft;
+pallinaTop=pallina.offsetTop;
 speed=1;
-step=0;
+
+stept=0;
+stepl=0;
 id=0;
 direzione=0; // 0 sopra
+
+
 function avvio(){
+    stepl=pallinaLeft;
     startB.disabled=true;
     if(direzione == 0)
     {id=setInterval(movimento,1);}
@@ -16,18 +23,39 @@ function avvio(){
 }
 function movimento(){
     direzione=0;
-    step+=speed;
-    document.getElementById("pallina").style.top = step+"px";
-    document.getElementById("pallina").style.left = step+"px";
-    if(step > tela.offsetHeight-pallina.offsetWidth){clearInterval(id); id1=setInterval(movimentoDown,1)}
+    stept+=speed;
+    stepl+=speed;
+    console.log(pallinaLeft);
+    document.getElementById("pallina").style.top= stept+"px";
+    document.getElementById("pallina").style.left = stepl+"px";
+    if(stepl > tela.offsetWidth-pallina.offsetWidth){clearInterval(id); id1=setInterval(movimentoDown,1)}
 }
 function movimentoDown(){
     direzione=1;
-    step-=speed;
-    console.log(step)
-    document.getElementById("pallina").style.top = step+"px";
-    document.getElementById("pallina").style.left = step+"px";
-    if(step <= 0){clearInterval(id1),id=setInterval(movimento,1)}
+    stepl-=speed;
+    stept+=speed;
+    document.getElementById("pallina").style.top = stept+"px";
+    document.getElementById("pallina").style.left = stepl+"px";
+    if(stept > 450){clearInterval(id1),id3=setInterval(movimentoLUP,1)}
+}
+function movimentoLUP(){
+    direzione=0;
+    stept-=speed;
+    stepl-=speed;
+    
+    document.getElementById("pallina").style.top= stept+"px";
+    document.getElementById("pallina").style.left = stepl+"px";
+    if(stepl == 0){ console.log("Ciao");clearInterval(id3); id4=setInterval(movimentoRUP,1)}
+}
+function movimentoRUP(){
+    direzione=0;
+    stept-=speed;
+    stepl+=speed;
+    
+    console.log(pallinaLeft);
+    document.getElementById("pallina").style.top= stept+"px";
+    document.getElementById("pallina").style.left = stepl+"px";
+    if(stept <= 0){clearInterval(id4); id=setInterval(movimento,1)}
 }
 function stop(){
     startB.disabled=false;
