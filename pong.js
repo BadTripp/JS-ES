c4e503aad2d3f0991309246531a85c7ba264c800
BadTripp <renato.turetta00@gmail.com>
2022-03-29 12:17:59 +0200

    Fix direzione

--- pong.js
+++ pong.js
@@ -1,12 +1,32 @@
 pallina=document.getElementById("pallina");
 tela=document.getElementById("tela");
+stopB=document.getElementById("buttonStop");
 step=0;
 id=0;
-function caduta(){
-    id=setInterval(movimentoY,10);
+direzione=0; // 0 sopra
+function avvio(){
+    if(direzione == 0)
+    {id=setInterval(movimento,1);}
+    else{
+        id1=setInterval(movimentoDown,1);
+    }
 }
-function movimentoY(){
+function movimento(){
+    direzione=0;
     step+=1;
     document.getElementById("pallina").style.top = step+"px";
-    if(pallina.offsetTop > tela.offsetHeight-pallina.offsetWidth){clearInterval(id)}
+    document.getElementById("pallina").style.left = step+"px";
+    if(step > tela.offsetHeight-pallina.offsetWidth){clearInterval(id); id1=setInterval(movimentoDown,1)}
+}
+function movimentoDown(){
+    direzione=1;
+    step-=1;
+    console.log(step)
+    document.getElementById("pallina").style.top = step+"px";
+    document.getElementById("pallina").style.left = step+"px";
+    if(step <= 0){clearInterval(id1),id=setInterval(movimento,1)}
+}
+function stop(){
+    clearInterval(id);
+    clearInterval(id1);
 }
