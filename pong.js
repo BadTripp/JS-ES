96141f0f1ca92cb8325820a9d79233b70bdb1703
BadTripp <renato.turetta00@gmail.com>
2022-03-29 12:27:43 +0200

    add Speed

--- pong.js
+++ pong.js
@@ -2,6 +2,7 @@ pallina=document.getElementById("pallina");
 tela=document.getElementById("tela");
 stopB=document.getElementById("buttonStop");
 startB=document.getElementById("button");
+speed=1;
 step=0;
 id=0;
 direzione=0; // 0 sopra
@@ -15,14 +16,14 @@ function avvio(){
 }
 function movimento(){
     direzione=0;
-    step+=1;
+    step+=speed;
     document.getElementById("pallina").style.top = step+"px";
     document.getElementById("pallina").style.left = step+"px";
     if(step > tela.offsetHeight-pallina.offsetWidth){clearInterval(id); id1=setInterval(movimentoDown,1)}
 }
 function movimentoDown(){
     direzione=1;
-    step-=1;
+    step-=speed;
     console.log(step)
     document.getElementById("pallina").style.top = step+"px";
     document.getElementById("pallina").style.left = step+"px";
@@ -32,4 +33,7 @@ function stop(){
     startB.disabled=false;
     clearInterval(id);
     clearInterval(id1);
+}
+function changeSpeed(controllo){
+    (controllo == 0) ? speed+=1 : speed-=1; 
 }
